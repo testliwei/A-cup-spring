ffb5702cecbdc90375689122febdc1255e461bb9
liweistudy <1719953921@qq.com>
2021-03-08 17:54:13 +0800

    autotest

---
+++ liweitest/src/main/java/com/example/liweitest/LiweitestApplication.java
@@ -0,0 +1,13 @@
+package com.example.liweitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiweitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiweitestApplication.class, args);
+	}
+
+}
---
+++ liweitest/src/test/java/com/example/liweitest/LiweitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liweitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiweitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
